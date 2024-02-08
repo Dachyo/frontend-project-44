e69db874eff5fa2d123d8f1bd46c1bc329ba738e
Dachyo <148446092+Dachyo@users.noreply.github.com>
2024-02-08 09:31:07 +0300

    Ame

--- bin/games/brain-even.js
+++ bin/games/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-import user from '../src/cli.js';
+import user from '../../src/cli.js';
 
 const Ame = () => {
   console.log('Welcome to the Brain Games!');
---
+++ src/index.js
@@ -0,0 +1,32 @@
+import readlineSync from 'readline-sync';
+import user from './cli.js';
+
+const userName = user();
+
+const randomQuestion = (defGame) => {
+  const [question, correctAnswer] = defGame();
+  console.log(`Question: ${question}`);
+  const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
+  return [userAnswer, correctAnswer, correctAnswer === userAnswer];
+};
+const getRandomNumber = (max, min = 0) => Math.floor(Math.random() * (max - min) + min);
+
+const games = (rules, defGame) => {
+  console.log(rules);
+  for (let i = 0; i < 3; i += 1) {
+    const [userAnswer, correctAnswer, answer] = randomQuestion(defGame);
+    if (answer) {
+      console.log('Correct!');
+    } else {
+      console.log(
+        `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \n
+        Let's try again, ${userName}!`,
+      );
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export { games, getRandomNumber };
+

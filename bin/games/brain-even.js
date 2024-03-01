d7176a2ef5ab58438f1ba36a314f9549ee5a5320
Dachyo <148446092+Dachyo@users.noreply.github.com>
2024-03-01 12:33:47 +0300

    Ame

--- bin/games/brain-even.js
+++ bin/games/brain-even.js
@@ -1,35 +1,31 @@
 #!/usr/bin/env node
-
 import readlineSync from 'readline-sync';
 import user from '../../src/cli.js';
+import { generateRandomNumber } from '../../src/index.js';
 
-const eveng = () => {
+const evenOrOdd = () => {
   const name = user();
+  let result = 0;
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-  let correctAnswersCount = 0;
-
-  while (correctAnswersCount < 3) {
-    const number = Math.floor(Math.random() * 100) + 1;
-    console.log(`Question: ${number}`);
-    const answer = readlineSync.question('Your answer: ');
-
-    const isEven = number % 2 === 0;
-    const isCorrect = (isEven && answer === 'yes') || (!isEven && answer === 'no');
-
-    if (isCorrect) {
-      console.log('Correct!');
-      correctAnswersCount += 1;
+  while (result < 3) {
+    const random = generateRandomNumber(1, 100);
+    console.log(`Question: ${random}`);
+    const answer = readlineSync.question('Answer: ');
+    if ((random % 2 === 0 && answer === 'yes') || (random % 2 !== 0 && answer === 'no')) {
+      console.log('Correct');
+      result += 1;
     } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${isEven ? 'yes' : 'no'}'.`);
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was ${random % 2 === 0 ? 'yes' : 'no'}`);
       console.log(`Let's try again, ${name}!`);
       break;
     }
   }
 
-  if (correctAnswersCount === 3) {
+  if (result === 3) {
     console.log(`Congratulations, ${name}!`);
   }
 };
 
-eveng();
+evenOrOdd();
+
+export default evenOrOdd;
